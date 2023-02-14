9ef7b0683fc529f8849876a2d469c47f306d1c33
Leke Mula <lekemula@Fatjonas-MBP.fritz.box>
2023-02-14 22:17:39 +0100

    Remove redundant copy_shopify.sh

--- copy_shopify.zsh
+++
@@ -1,4 +0,0 @@
-cp .gitignore_global ~/shopify-dotfiles/personal/.gitignore_global
-cp .vimrc ~/shopify-dotfiles/personal/.vimrc
-cp .p10k.zsh ~/shopify-dotfiles/personal/.p10k.zsh
-cp coc-settings.json ~/shopify-dotfiles/personal/coc-settings.json
