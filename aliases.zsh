d376ecfa6edededc4a88beefc7edf7ffe2342d60
Lekë Mula <leke.mula@gmail.com>
2023-06-22 14:19:48 +0200

    Add alias for reloading zshell

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,6 @@
 alias gcofzf='git checkout $(git branch --all | fzf)'
 alias gcobfzf='git checkout -b $(git branch --all | fzf)'
 alias gcob='git checkout -b'
-alias mkdir='mkdir -p'
+alias mkdir='mkdir -p' # create dir recursively
 alias gdb='git diff origin/main..HEAD'
+alias res='omz reload' # reload zsh
