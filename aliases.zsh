903fc3c6063d330e4c29cc0f0f73e65dc1df73fb
Lekë Mula <l.mula@finlink.de>
2023-11-28 11:52:03 +0100

    Add copilot and rspec changed files aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,3 +4,9 @@ alias gcob='git checkout -b'
 alias mkdir='mkdir -p' # create dir recursively
 alias gdb='git diff origin/main..HEAD'
 alias res='omz reload' # reload zsh
+alias lsport='lsof -i -P -n' # list ports
+alias '??=gh copilot explain'
+alias '???=gh copilot suggest'
+
+alias lm_rspec_changed='bundle exec rspec $(git status -s | awk '\''{ print $2  }'\'' | grep spec | xargs echo)'
+
