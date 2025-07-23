e75befe80ad82454c1861dbfd5f7a727715adc6f
Lekë Mula <l.mula@finlink.de>
2025-07-23 20:38:34 +0200

    Retire CopilotChat plugin in favour of avante

--- nvim/lua/copilot_settings.lua
+++ nvim/lua/copilot_settings.lua
@@ -12,29 +12,3 @@ require("copilot").setup({
     },
   }
 })
-
-require("CopilotChat").setup {
-  mappings = {
-    reset = {
-      normal = '<leader>air',
-      insert = '<leader>air',
-    }
-  },
-  prompts = {
-    YARD = {
-      prompt = 'Add Ruby Yard Documentation',
-      -- system_prompt = 'You are very good at explaining stuff',
-      mapping = '<leader>aiyd',
-      description = 'Add Ruby Yard Documentation',
-    }
-  }
-}
-
-vim.cmd('nnoremap <leader>tai :CopilotChatToggle<CR>')
-vim.cmd('nnoremap <leader>aic :CopilotChat ')
-vim.cmd('vnoremap <leader>aic :CopilotChat ')
-vim.cmd('vnoremap <leader>aie :CopilotChatExplain<CR>')
-vim.cmd('nnoremap <leader>ais :CopilotChatSave ')
-vim.cmd('nnoremap <leader>aiS :CopilotChatStop<CR>')
-vim.cmd('nnoremap <leader>aip :CopilotChatPrompts<CR>')
-vim.cmd('vnoremap <leader>aip :CopilotChatPrompts<CR>')
