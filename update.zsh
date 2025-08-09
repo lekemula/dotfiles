36815fe2f1d4c380e99c8c7a61c935c30bfcdc66
Lekë Mula <l.mula@finlink.de>
2025-08-09 19:05:47 +0200

    Add bundle_update_interactive

--- update.zsh
+++ update.zsh
@@ -4,6 +4,6 @@ gh extension upgrade --all
 ~/.tmux/plugins/tpm/bin/update_plugins all
 # antigen update - FIXME: this command is not found when running this script
 gem update --system
-gem update solargraph solargraph-rspec ruby-debug-ide ripper-tags gem-ripper-tags vernier profile-viewer 
+gem update solargraph solargraph-rspec ruby-debug-ide ripper-tags gem-ripper-tags vernier profile-viewer bundle_update_interactive
 npm update -g
 curl https://raw.githubusercontent.com/junegunn/fzf-git.sh/main/fzf-git.sh > fzf-git.sh
